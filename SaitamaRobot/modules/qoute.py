70aa664b62d87092d679e6d8629344d9c6d4293e
noor-ali-346 <73874131+noor-ali-346@users.noreply.github.com>
2020-12-16 01:52:17 +1300

    Update qoute.py

--- SaitamaRobot/modules/qoute.py
+++ SaitamaRobot/modules/qoute.py
@@ -1,5 +1,3 @@
-# Oringinal Source from Nicegrill: https://github.com/erenmetesar/NiceGrill/
-# Ported to Lynda by: @pokurt
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import types, functions
 from fontTools.ttLib import TTFont 
