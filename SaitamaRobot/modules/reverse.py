62d7c89347a1c83da27584911eb743ac5edb33a6
Koro-Sensei-0 <75716522+Koro-Sensei-0@users.noreply.github.com>
2021-01-17 19:30:43 +0530

    Update reverse.py

--- SaitamaRobot/modules/reverse.py
+++ SaitamaRobot/modules/reverse.py
@@ -92,7 +92,7 @@ def reverse(update: Update, context:CallbackContext):
         fetchUrl = response.headers['Location']
 
         if response != 400:
-            xx = bot.send_message(chat_id, "Image was successfully uploaded to Google."
+            xx = bot.send_message(chat_id, "Chaliyeh chutiyapa karte hai!."
                                   "\nParsing it, please wait.", reply_to_message_id=rtmid)
         else:
             xx = bot.send_message(chat_id, "Google told me to go away.", reply_to_message_id=rtmid)
@@ -107,14 +107,14 @@ def reverse(update: Update, context:CallbackContext):
             imgspage = match['similar_images']
 
         if guess and imgspage:
-            xx.edit_text(f"[{guess}]({fetchUrl})\nProcessing...", parse_mode='Markdown', disable_web_page_preview=True)
+            xx.edit_text(f"[{guess}]({fetchUrl})\nNoobde log waifu ka nam tak nahi pata..", parse_mode='Markdown', disable_web_page_preview=True)
         else:
             xx.edit_text("Couldn't find anything.")
             return
 
         images = scam(imgspage, lim)
         if len(images) == 0:
-            xx.edit_text(f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})"
+            xx.edit_text(f"[{guess}]({fetchUrl})\n[Yeh lo chutiyo karo protecc]({imgspage})"
                           "\nCouldn't fetch any images.", parse_mode='Markdown', disable_web_page_preview=True)
             return
 
